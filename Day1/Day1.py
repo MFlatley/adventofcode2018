214e3431609aefad866d229438e829c35c71c2ae
Mike Flatley <flatley.art@gmail.com>
2018-12-03 14:18:11 -0600

    Putting loop in function to have return value

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,18 +1,22 @@
-finalOutput = 0
-thisList = []
 inputList = []
 fileName = ".\\adventofcode2018\\Day1\\input.txt"
 def calibrateOutput (input, output):
     output = output + input
     return output;
 
+def findRepeatedFrequency(providedList):
+    processedList = []
+    frequency = 0
+    
+    for change in providedList:
+        processedList.append(frequency)
+        frequency = calibrateOutput(change, frequency)
+        if frequency in processedList:
+            return frequency;
+
 file = open(fileName, "r")
-for line in file:
+for line in file:    
     inputList.append(int(line))
 file.close();
 
-while  finalOutput not in thisList:    
-    for change in inputList:
-        thisList.append(finalOutput)
-        finalOutput = calibrateOutput(change,finalOutput)
-print(finalOutput)
+print(findRepeatedFrequency(inputList))
