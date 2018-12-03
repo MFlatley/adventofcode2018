5142ab9dcf3a4013b6648ea220f5e61007ea2fc8
Mike Flatley <flatley.art@gmail.com>
2018-12-03 14:51:49 -0600

    Adding Day 2 files

---
+++ Day2/Day2.py
@@ -0,0 +1,22 @@
+inputList = []
+fileName = ".\\adventofcode2018\\Day1\\input.txt"
+def calibrateOutput (input, output):
+    output = output + input
+    return output;
+
+def findRepeatedFrequency(providedList):
+    processedList = []
+    frequency = 0
+    while True:
+        for change in providedList:
+            processedList.append(frequency)
+            frequency = calibrateOutput(change, frequency)
+            if frequency in processedList:
+                return frequency;
+
+file = open(fileName, "r")
+for line in file:    
+    inputList.append(int(line))
+file.close();
+
+print(findRepeatedFrequency(inputList))
