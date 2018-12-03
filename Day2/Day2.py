41ffe5e198e6699ced412a611d459f78cd0a9100
Mike Flatley <flatley.art@gmail.com>
2018-12-03 15:22:23 -0600

    Completed part 1

--- Day2/Day2.py
+++ Day2/Day2.py
@@ -1,22 +1,34 @@
+doubleTracker = 0
+tripleTracker = 0
 inputList = []
-fileName = ".\\adventofcode2018\\Day1\\input.txt"
-def calibrateOutput (input, output):
-    output = output + input
-    return output;
+fileName = ".\\adventofcode2018\\Day2\\input.txt"
+def findDoubles (input):
+    doubleFound = False
+    for letter in input:
+        if input.count(letter) == 2:
+            doubleFound = True
+    if doubleFound:
+        return 1
+    else: 
+        return 0
 
-def findRepeatedFrequency(providedList):
-    processedList = []
-    frequency = 0
-    while True:
-        for change in providedList:
-            processedList.append(frequency)
-            frequency = calibrateOutput(change, frequency)
-            if frequency in processedList:
-                return frequency;
+def findTriples (input):
+    tripleFound = False
+    for letter in input:        
+        if input.count(letter) == 3:
+            tripleFound = True;
+    if tripleFound:
+        return 1
+    else: 
+        return 0
 
 file = open(fileName, "r")
 for line in file:    
-    inputList.append(int(line))
+    inputList.append(line)
 file.close();
 
-print(findRepeatedFrequency(inputList))
+for line in inputList:
+    doubleTracker += findDoubles(line)
+    tripleTracker += findTriples(line)
+
+print(doubleTracker*tripleTracker)
